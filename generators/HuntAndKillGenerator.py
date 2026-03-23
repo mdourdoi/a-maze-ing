5d6147e9bde6657be1f911ea99289e6a6ac3252a
Matthieu Dourdoigne <mdourdoi@z4r4p2.42lyon.fr>
2026-03-23 16:45:46 +0100

    wip refacto semi-private attributes & methods

--- generators/HuntAndKillGenerator.py
+++ generators/HuntAndKillGenerator.py
@@ -15,32 +15,32 @@ class HuntAndKillGenerator(MazeGenerator):
 
     def _generate_maze(self) -> Generator:
         cur_x, cur_y = self.maze.entry[0], self.maze.entry[1]
-        self.maze.body[cur_y][cur_x].visit()
+        self.maze.body[cur_y][cur_x]._visit()
         running = True
-        self.maze.body[cur_y][cur_x].visit()
+        self.maze.body[cur_y][cur_x]._visit()
         while running:
-            valid_neighbours = self.maze.get_valid_neighbours(cur_x, cur_y)
+            valid_neighbours = self.maze._get_valid_neighbours(cur_x, cur_y)
             if valid_neighbours:
                 next_dir = self.random.choice(list(valid_neighbours.keys()))
                 self._carve(cur_x, cur_y, next_dir)
                 chosen = valid_neighbours[next_dir]
                 cur_x, cur_y = chosen[0], chosen[1]
-                self.maze.body[cur_y][cur_x].visit()
+                self.maze.body[cur_y][cur_x]._visit()
                 yield [cur_x, cur_y]
             else:
                 running = False
                 for j in range(self.height):
                     for i in range(self.wid):
-                        if (not self.maze.body[j][i].is_visited
+                        if (not self.maze.body[j][i]._is_visited
                                 and not self.maze.body[j][i].is_ft):
-                            vis_neigh = self.maze.get_visited_neighbours(i, j)
+                            vis_neigh = self.maze._get_visited_neighbours(i, j)
                             if vis_neigh:
                                 running = True
                                 next_dir = self.random.choice(
                                     list(vis_neigh.keys()))
                                 self._carve(i, j, next_dir)
                                 cur_x, cur_y = i, j
-                                self.maze.body[cur_y][cur_x].visit()
+                                self.maze.body[cur_y][cur_x]._visit()
                                 yield [cur_x, cur_y]
                         if running is True:
                             break
--- generators/PrimGenerator.py
+++ generators/PrimGenerator.py
@@ -15,21 +15,21 @@ class PrimGenerator(MazeGenerator):
 
     def _generate_maze(self) -> Generator:
         cur_x, cur_y = self.maze.entry[0], self.maze.entry[1]
-        self.maze.body[cur_y][cur_x].visit()
+        self.maze.body[cur_y][cur_x]._visit()
         frontier = {
             tuple(value) for value in list(
-                self.maze.get_valid_neighbours(
+                self.maze._get_valid_neighbours(
                     cur_x, cur_y).values())}
         while frontier:
             x, y = self.random.choice(sorted(frontier))
-            self.maze.body[y][x].visit()
+            self.maze.body[y][x]._visit()
             direction = self.random.choice(
-                list(self.maze.get_visited_neighbours(x, y)))
+                list(self.maze._get_visited_neighbours(x, y)))
             self._carve(x, y, direction)
             frontier.remove((x, y))
             new_frontier = {
                 tuple(value) for value in list(
-                    self.maze.get_valid_neighbours(
+                    self.maze._get_valid_neighbours(
                         x, y).values())}
             frontier = frontier | new_frontier
             yield [x, y]
